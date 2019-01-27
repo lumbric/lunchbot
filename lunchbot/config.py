2d95c23cc12e0044040b85ebd52e57bdf22e2953
lumbric <lumbric@gmail.com>
2019-01-27 20:20:41 +0100

    Add config relative to Git root

--- lunchbot/config.py
+++ lunchbot/config.py
@@ -1,5 +1,6 @@
 import yaml
 import os.path as op
+from pathlib import Path
 
 
 # will try to find the first folder
@@ -7,16 +8,31 @@ CONFIG_FILE_PATHS = [
     op.join(op.dirname(__file__), '..', 'config'),
     op.join(op.expanduser('~'), '.config', 'lunchbot'),
     '/etc/lunchbot',
+    # one more added at runtime!
 ]
+
+def _find_git_root():
+    this_file_dir = Path(op.abspath(op.dirname( __file__ )))
+    for folder in reversed(this_file_dir.parents):
+        if op.isdir(op.join(folder, '.git')):
+            return folder
 
 
 def _get_cfg_dir():
-    for cfg_path in CONFIG_FILE_PATHS:
+    # add path relative to GIT root, useful when running from GIT clone
+    git_root = _find_git_root()
+    if git_root is None:
+        config_file_paths = CONFIG_FILE_PATHS
+    else:
+        config_file_paths = (CONFIG_FILE_PATHS +
+                             [op.join(git_root, 'config')])
+
+    for cfg_path in config_file_paths:
         if op.isdir(cfg_path):
             return cfg_path
     else:
-        raise RuntimeError("No config file path not found "
-                           "(search paths: {CONFIG_FILE_PATHS})")
+        raise RuntimeError(f"No config file path not found "
+                           "(search paths: {config_file_paths})")
 
 
 def slack_api_token():
