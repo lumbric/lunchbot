51798e0e45c77b6009a993027c41ce7d09530c15
lumbric <lumbric@gmail.com>
2019-12-21 15:12:55 +0100

    Filter text for malicious characters
    Probably not very important, but thinking about security is never a bad
    idea.

--- lunchbot/main.py
+++ lunchbot/main.py
@@ -1,11 +1,21 @@
+import re
 from datetime import datetime
 
 from lunchbot.scrape_mensa import read_day_menu
 from lunchbot.slack_api import post_menu
 
 
+def filter_malicious(text):
+    """We don't want the mensa page to be able to allow arbitrary formating in our slack channel.
+    emojis are allowed, but except of that only a very limited set of characters."""
+    return re.sub(r"[^\w €\-,\(\)\:]", "", text)
+
+
 def format_menus(menus):
-    """Prefix every menu item with a numbered emoji reaction.
+    """Format dictionary of menu items for slack to one string.
+
+    This function was originally intended to add numbered emoji icons before each meal, so one
+    could vote for them, but this feature has been removed.
 
     Parameters
     ----------
@@ -14,12 +24,13 @@ def format_menus(menus):
         and item is one meal with price as str
 
     """
+    # In theory this could be very large and produce a harmful message, but let's trust on slack
+    # here and not care too much.
+
     menu = ''
-    idx = 0
     for name, items in menus.items():
-        menu += f"\n*{name}*\n"
-        menu += "".join(f" - {item}\n" for item in items)
-        idx += len(items)
+        menu += f"\n*{filter_malicious(name)}*\n"
+        menu += "".join(f" - {filter_malicious(item)}\n" for item in items)
 
     # menu contains a leading and trailing new line, but slack doesn't care
 
---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import pytest
+from lunchbot.main import format_menus
+from lunchbot.main import filter_malicious
+
+
+def test_filter_malicious():
+    good_strings = [
+        'BOKU Wien M-Cafe Mendel',
+        'Käse-Leberkäse-Semmel € 2,50',
+        'Wok Gemüse in Koi Soy Sauce :cucumber: mit Reis (A,L,D,F) € 5,00'
+    ]
+    for good_string in good_strings:
+        assert filter_malicious(good_string) == good_string
+    assert filter_malicious('\\evilcommand') == 'evilcommand'
+    assert filter_malicious('*evilcommand*') == 'evilcommand'
+
